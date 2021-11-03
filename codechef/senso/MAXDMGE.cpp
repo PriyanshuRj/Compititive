5fc1cfeb43bc7f52b897e56fbd8ee5a77e239c47
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-03 21:03:05 +0530

    added new code

--- codechef/senso/MAXDMGE.cpp
+++ codechef/senso/MAXDMGE.cpp
@@ -1,5 +1,29 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n],arr2[n];
+        for(int i =0;i<n;i++) cin>>arr[i];
+        for(int i =0;i<n;i++){
+            int maxand = 0;
+            int val = arr[i];
+            for(int j=i+1;j<n;j++) {
+                val = val&arr[j];
+                if(val>maxand) maxand = val;
+            }
+            val = arr[i];
+            for(int j = i-1;j>=0;j--){
+                val = val&arr[j];
+                if(val>maxand) maxand = val;                
+            }
+            arr2[i] = maxand;
+        }
+        for(int i =0;i<n;i++) cout<<arr2[i]<<" ";
+        cout<<endl;
+
+    }
 }
