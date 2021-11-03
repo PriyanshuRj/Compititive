7dedf8803ea930d443186a7c3a55d65d89bc5445
priyanshu-rajput-01 <priyanshurajput0071109@gmail.com>
2021-11-03 21:21:56 +0530

    Updated Q5

---
+++ prepbytes/practice/5.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--){
+    long long num;
+    cin>>num;
+    while(num>1)
+    {
+      cout<<num<<" ";
+      if(num%2==0) num = pow(num,0.5);
+      else num = pow(num,1.5);
+      
+    }
+    if(num ==1)cout<<num;
+    cout<<endl;
+  }
+  
+  return 0;
+}
