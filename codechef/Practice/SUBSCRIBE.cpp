669a0eed6f9244fb60f6fd942a5c26b7e19b14b8
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-05-14 11:08:25 +0530

    Added SUBSCRIBE problem

---
+++ codechef/Practice/SUBSCRIBE.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int it =0;it<t;it++){
+        int num;
+        cin>>num;
+        if(num<=30) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
