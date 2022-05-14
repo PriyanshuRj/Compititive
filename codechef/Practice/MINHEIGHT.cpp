6926314cf627f848be3fe8eaf7e0161675764378
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-05-14 12:01:21 +0530

    Added MIN-HEIGHT problem

--- codechef/Practice/MINHEIGHT.cpp
+++ codechef/Practice/MINHEIGHT.cpp
@@ -6,8 +6,8 @@ int main(){
     for(int it =0;it<t;it++){
         int a,b;
         cin>>a>>b;
-        if(a>b) cout<<"A\n";
-        else cout<<"B\n";
+        if(a>=b) cout<<"YES\n";
+        else cout<<"NO\n";
     }
     return 0;
 }
